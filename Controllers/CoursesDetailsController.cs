47038e052e2e79b1fb31e7a42beb7de9a9e60690
Hao CN <caonguyenhao69@gmail.com>
2025-02-21 17:44:25 +0700

    Edit Courses, Add CoursesDetails, Add CoursesDetailsController, Edit _Layout

---
+++ Controllers/CoursesDetailsController.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using GreTutor.Models;
+
+namespace GreTutor.Controllers;
+
+public class CoursesDetailsController : Controller
+{
+    public IActionResult Courses()
+    {     
+        return View();
+    }
+    public IActionResult CoursesDetails1()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails2()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails3()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails4()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails5()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails6()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails7()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails8()
+    {
+        return View();
+    }
+    public IActionResult CoursesDetails9()
+    {
+        return View();
+    }
+
+}
--- obj/Debug/net7.0/GreTutor.AssemblyInfo.cs
+++ obj/Debug/net7.0/GreTutor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreTutor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05b1a2cf45730ae25fff6c7268219222deb9c333")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreTutor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreTutor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
